8d5282b4edcca369cbc5b7fa271c55fbc00001f5
Luca <lucacostacte@icloud.com>
2025-09-30 21:16:52 -0300

    Update

---
+++ person.java
@@ -0,0 +1,32 @@
+public abstract class person {
+    protected String name;
+    protected String address;
+    protected String telephone;
+    
+    public abstract void validateData();
+    public abstract String getType();
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getTelephone() {
+        return telephone;
+    }
+    
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
