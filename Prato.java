6965ef64efcd71b6bb827fefc6fbe28c1d332501
Luca <lucacostacte@icloud.com>
2025-10-31 15:17:25 -0300

    Update
    fix

---
+++ Prato.java
@@ -0,0 +1,45 @@
+public class Prato {
+    private String nome;
+    private double preco;
+    private String descricao;
+    private int tempoEstimadoEntrega;
+
+    public Prato(String nome, double preco, String descricao, int tempoEstimadoEntrega) {
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+        this.tempoEstimadoEntrega = tempoEstimadoEntrega;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getTempoEstimadoEntrega() {
+        return tempoEstimadoEntrega;
+    }
+
+    public void setTempoEstimadoEntrega(int tempoEstimadoEntrega) {
+        this.tempoEstimadoEntrega = tempoEstimadoEntrega;
+    }
+}
